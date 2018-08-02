8880aee63ec72327c29b81e09d548a30ffef74a9
杨旸 <yangyang@qingting.fm>
2018-08-02 19:03:15 +0800

    change to vue

--- src/pages/Other/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class OtherComponent extends React.PureComponent<{}, {}> {
-    render() {
-        return <h2>Other</h2>
-    }
-}
-
-export default OtherComponent;
--- src/pages/Theater/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class TheaterComponent extends React.PureComponent<{}, {}> {
-    render() {
-        return <h2>Theater</h2>
-    }
-}
-
-export default TheaterComponent;
